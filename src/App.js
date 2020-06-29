101fe395a60f260b06f01841f021ff1b424bc103
Kevin Pallikunnel <oapallikunnel@gmail.com>
2020-06-28 19:09:14 -0500

    needed to remove home from app.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,11 @@ import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import {Bio} from './Components/Bio/Bio';
 import {Projects} from './Components/Projects/Projects';
-import Home from './Components/Home/Home';
 import {Experience} from './Components/Experience/Experience';
 import Footer from './Components/Footer/Footer';
 import {AboutMe} from './Components/AboutMe/AboutMe';
 import 'bootstrap/dist/css/bootstrap.css';
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentPage: 'work',
-    };
-  }
-
   render() {
     return (
       <div className="App">
