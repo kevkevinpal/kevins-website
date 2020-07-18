061052540f99602c8f6736de75fd67fe47ddb7ec
Kevin Pallikunnel <oapallikunnel@gmail.com>
2020-07-18 07:44:36 -0500

    removed the useless content on bottom nav

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,9 +5,6 @@ class Footer extends React.Component {
   render() {
     return (
       <div class="d-md-flex justify-content-around align-items-center Footer-Row">
-        <div class="col-12 col-md-auto">Test</div>
-        <div class="col-12 col-md-auto">oapallikunnel@gmail.com</div>
-        <div class="col-12 col-md-auto">Social Media</div>
       </div>
     );
   }
